1cfc70ca05af31fccd0a5d6e4288badc4fa46082
Kristin Lam <klam623@gmail.com>
2022-08-17 18:55:31 -0400

    feat: create about page and add to navbar

---
+++ client/components/About.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Welcome to Mei Crochet!</h1>
+      <p>
+        As a software engineer and aspiring crocheter, I wanted to practice
+        designing and building an e-commerce site while promoting my favorite
+        crochet patterns.
+      </p>
+      <p>
+        Feel free to add items to your cart and checkout. No account or credit
+        card needed, this site’s just for fun! On checkout, you’ll receive a
+        summary with links to all the crochet patterns you wanted, which you can
+        visit if you'd like to purchase them for real. There are plenty of free
+        patterns to enjoy as well.
+      </p>
+      <p>
+        Crochet is a wonderful way to unwind, reflect, and slow down. It brings
+        together a lot of great things — a community of creative artists, an
+        appreciation for slow (sometimes *really* slow) fashion, and handmade
+        gifts from the heart. Nothing says "I love you" like spending hours
+        picking out the perfect yarn and colors, crocheting alongside a true
+        crime Youtube playlist, and weaving in dozens of ends. Happy crocheting!
+      </p>
+      <p>With much love, Kristin</p>
+    </div>
+  );
+};
+
+export default About;
