d1234b2d0b3ca1a1901280db6d58f918f41f96c1
Kristin Lam <klam623@gmail.com>
2022-08-17 18:30:38 -0400

    create routes for creating, updating, and deleting patterns

--- server/api/patterns.js
+++ server/api/patterns.js
@@ -26,7 +26,31 @@ router.get('/:id', async (req, res, next) => {
 
 // Admin Routes
 // POST /api/patterns/
-
-// DELETE /api/patterns/:id
+router.post('/', async (req, res, next) => {
+  try {
+    res.status(201).json(await Pattern.create(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
 
 // PUT /api/patterns/:id
+router.put('/:id', async (req, res, next) => {
+  try {
+    const pattern = await Pattern.findByPk(req.params.id);
+    res.json(await pattern.update(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /api/patterns/:id
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const pattern = await Pattern.findByPk(req.params.id);
+    await pattern.destroy();
+    res.json(pattern);
+  } catch (err) {
+    next(err);
+  }
+});
