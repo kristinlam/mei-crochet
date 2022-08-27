00949f71d84bb144adece039d63b95f260908428
Kristin Lam <klam623@gmail.com>
2022-08-27 11:48:26 -0400

    style: Home component

--- client/components/Home.js
+++ client/components/Home.js
@@ -33,28 +33,34 @@ const Home = () => {
 
   return (
     <div>
-      <header className="flex">
-        <div className="bg-pink flex flex-col justify-center items-center	flex-1">
-          <h1>Welcome to Mei Crochet</h1>
-          <h2>Find your next crochet project</h2>
+      <header className="lg:flex">
+        <div className="bg-pink py-10 flex flex-col justify-center items-center	text-center lg:w-1/2">
+          <h1 className="mb-4">Find your next crochet project</h1>
+          <p className="mb-6 text-xl">
+            Beautiful patterns designed by talented artists
+          </p>
           <button className="bg-green rounded-full border-2	border-black px-5 py-2">
             Shop now
           </button>
         </div>
-        <div className="flex-1">
-          <img alt="header" src="./images/header.jpg" />
+        <div className="lg:w-1/2">
+          <img
+            className="object-cover"
+            alt="header"
+            src="./images/header.jpg"
+          />
         </div>
       </header>
-      <div className="bg-green p-x-4">
-        <p>
+      <div className="bg-green py-6">
+        <p className="sm:text-xl text-white">
           Let's crochet the day away. Let's crochet the day away. Let's crochet
           the day away. Let's crochet the day away.
         </p>
       </div>
-      <section className="bg-beige flex justify-around">
+      <section className="bg-beige lg:flex lg:justify-around py-20">
         {featuredPatterns?.map((pattern) => (
-          <div key={pattern.id}>
-            <div className="w-96 h-96">
+          <div key={pattern.id} className="mb-10 lg:mb-0">
+            <div className="mx-auto w-96 h-96 lg:w-64 lg:h-64 xl:w-80 xl:h-80 mb-4 sm:mb-6">
               <img
                 className="h-full w-full object-cover border-8  border-orange rounded-2xl"
                 src={pattern.image}
@@ -64,17 +70,19 @@ const Home = () => {
           </div>
         ))}
       </section>
-      <section className="bg-orange flex flex-col items-center">
-        <p>
-          Feel free to add items to your cart and checkout. No account or credit
-          card needed, this site’s just for fun! On checkout, you’ll receive a
-          summary with links to all the crochet patterns you wanted, which you
-          can visit if you'd like to purchase them for real. There are plenty of
-          free patterns to enjoy too, so go crazy!
-        </p>
-        <button className="bg-green rounded-full border-2	border-black px-5 py-2">
-          Learn more
-        </button>
+      <section className="bg-orange flex flex-col items-center py-20">
+        <div className="w-5/6 lg:w-1/2">
+          <p>
+            Feel free to add items to your cart and checkout. No account or
+            credit card needed, this site’s just for fun! On checkout, you’ll
+            receive a summary with links to all the crochet patterns you wanted,
+            which you can visit if you'd like to purchase them for real. There
+            are plenty of free patterns to enjoy too, so go crazy!
+          </p>
+          <button className="bg-green rounded-full border-2	border-black px-5 py-2">
+            Learn more
+          </button>
+        </div>
       </section>
     </div>
   );
