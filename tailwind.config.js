b664a0f367c7dc8d8cacd2c9f27b12162d747295
Kristin Lam <klam623@gmail.com>
2022-08-26 22:15:57 -0400

    style: add colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
+    colors: {
+      blue: '#1fb6ff',
+      pink: '#E48B7A',
+      orange: '#EEA843',
+      beige: '#F7EDDD',
+      green: '#D3D4C4',
+      'gray-dark': '#273444',
+      gray: '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
