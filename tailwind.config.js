2f75cfea254b6a5c093cdd2a134ec5e9785fec56
Kristin Lam <klam623@gmail.com>
2022-08-27 20:12:16 -0400

    style: update colors and tailwind output css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,32 @@ module.exports = {
       pink: '#E48B7A',
       orange: '#EEA843',
       beige: '#F7EDDD',
-      green: '#D3D4C4',
+      green: '#CAD62F',
       'gray-dark': '#273444',
       gray: '#8492a6',
       'gray-light': '#d3dce6',
+      white: '#fff',
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
     },
-    extend: {},
+    extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
