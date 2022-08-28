db48210f83b639715d18cec0c6e30c235a0b4c46
Kristin Lam <klam623@gmail.com>
2022-08-28 03:25:09 -0400

    style: add global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,19 @@ module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
     colors: {
-      blue: '#1fb6ff',
-      pink: '#E48B7A',
-      orange: '#EEA843',
-      beige: '#F7EDDD',
-      green: '#CAD62F',
+      red: '#F8483F', // used
+      yellow: '#FCC575',
+      blue: '#C4DFEF', // used
+      pink: '#FBB7BA',
+      orange: '#F9B18B', // used
+      'orange-dark': '#F47A3B', // used
+      beige: '#F6E4C3', // used
+      green: '#ADB05A', // used
+      'green-light': '#CAD62F', // buttons
+      'green-medium': '#C2E19B',
+      purple: '#D3C2F8',
+      black: '#232323',
+
       'gray-dark': '#273444',
       gray: '#8492a6',
       'gray-light': '#d3dce6',
@@ -19,8 +27,8 @@ module.exports = {
     },
     extend: {
       animation: {
-        marquee: 'marquee 25s linear infinite',
-        marquee2: 'marquee2 25s linear infinite',
+        marquee: 'marquee 30s linear infinite',
+        marquee2: 'marquee2 30s linear infinite',
       },
       keyframes: {
         marquee: {
