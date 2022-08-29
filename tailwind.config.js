074c9fc457efb08fbed6cf5f63fd5198afad98e3
Kristin Lam <klam623@gmail.com>
2022-08-29 01:00:08 -0400

    style: update colors in theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,24 @@ module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
     colors: {
-      red: '#F8483F', // used
-      yellow: '#FCC575',
-      blue: '#C4DFEF', // used
-      pink: '#FBB7BA',
-      orange: '#F9B18B', // used
-      'orange-dark': '#F47A3B', // used
-      beige: '#F6E4C3', // used
-      green: '#ADB05A', // used
-      'green-light': '#CAD62F', // buttons
-      'green-medium': '#C2E19B',
-      purple: '#D3C2F8',
       black: '#232323',
-
-      'gray-dark': '#273444',
-      gray: '#8492a6',
-      'gray-light': '#d3dce6',
-      white: '#fff',
+      white: '#FFFFFF',
+      yellow: '#F4DE91',
+      beige: '#F6E4C3',
+      pink: {
+        100: '#F3C9B2',
+        200: '#FCB0B1',
+        300: '#F2789B',
+      },
+      orange: {
+        100: '#FCA824',
+        200: '#F47A3B',
+        300: '#E95B0B',
+      },
+      green: {
+        100: '#C3B000',
+        200: '#ADB05A',
+      },
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
