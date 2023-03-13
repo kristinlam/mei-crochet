fa39abcd52830cc33259d3a4d113326efd31bb2d
Kristin Lam <klam623@gmail.com>
2023-03-12 23:50:27 -0400

    Use CartContext to make Add to Cart btn functional

--- client/pages/PatternsPage.js
+++ client/pages/PatternsPage.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getPatterns } from '../store/patterns';
 import { Link } from 'react-router-dom';
+import CartContext from '../context/cart';
 import { convertCents } from '../helpers';
 import FullPageLayout from '../layouts/FullPageLayout';
 import Button from '../components/Button';
 
 const PatternsPage = () => {
+  const { cartItems, addItem } = useContext(CartContext);
   const patterns = useSelector((state) => state.patterns);
   const dispatch = useDispatch();
 
@@ -14,36 +16,40 @@ const PatternsPage = () => {
     dispatch(getPatterns());
   }, []);
 
+  const handleAddToCartClick = (pattern) => {
+    addItem(pattern);
+  };
+
   return (
     <FullPageLayout>
       <h1 className="text-orange-300 text-center mb-16">All Patterns</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-10">
         {patterns.map((pattern) => (
-          <Link key={pattern.id} to={`/shop/${pattern.id}`}>
-            <div>
+          <div key={pattern.id} className="text-center">
+            <Link key={pattern.id} to={`/shop/${pattern.id}`}>
               <img
                 className="object-cover w-full aspect-square rounded-2xl mb-4"
                 src={pattern.image}
               />
-              <div className="text-center">
-                <div className="mb-2">
-                  <h3>{pattern.name}</h3>
-                  <p>{pattern.creator}</p>
-                  <p>
-                    {pattern.price !== 0 ? convertCents(pattern.price) : 'Free'}
-                  </p>
-                </div>
-                <Button
-                  backgroundColor="bg-yellow-200"
-                  textColor="text-orange-200"
-                  border="2"
-                >
-                  Add to cart
-                </Button>
+
+              <div className="mb-2">
+                <h3>{pattern.name}</h3>
+                <p>{pattern.creator}</p>
+                <p>
+                  {pattern.price !== 0 ? convertCents(pattern.price) : 'Free'}
+                </p>
               </div>
-            </div>
-          </Link>
+            </Link>
+            <Button
+              backgroundColor="bg-yellow-200"
+              textColor="text-orange-200"
+              border="2"
+              onClick={() => handleAddToCartClick(pattern)}
+            >
+              Add to cart
+            </Button>
+          </div>
         ))}
       </div>
     </FullPageLayout>
