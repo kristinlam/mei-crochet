36ef8441b61bab64e89b9fd35d802b48e17706f7
Kristin Lam <klam623@gmail.com>
2023-03-13 22:12:49 -0400

    Style product details page

--- client/pages/SinglePatternPage.js
+++ client/pages/SinglePatternPage.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getSinglePattern } from '../store/singlePattern';
 import { convertCents } from '../helpers';
+import ProductListingLayout from '../layouts/ProductListingLayout';
+import Button from '../components/Button';
 
 const SinglePatternPage = (props) => {
   const pattern = useSelector((state) => state.singlePattern);
@@ -12,15 +14,41 @@ const SinglePatternPage = (props) => {
   }, []);
 
   return (
-    <div className="max-w-screen-2xl flex px-4 md:px-8">
-      <img src={pattern.image} />
-      <div>
-        <h1>{pattern.name}</h1>
-        <p>{pattern.price !== 0 ? convertCents(pattern.price) : 'Free'}</p>
-        <p>{pattern.creator}</p>
-        <p>{pattern.description}</p>
+    <ProductListingLayout>
+      <div className="lg:flex-1">
+        <img
+          className="w-full aspect-square object-cover rounded-2xl"
+          src={pattern.image}
+        />
       </div>
-    </div>
+
+      <div className="lg:flex-1 lg:px-12 mt-10">
+        <div className="flex items-start text-orange mb-2">
+          <div className="flex-grow pr-8 sm:pr-12 lg:pr-10">
+            <h1 className="text-4xl leading-snug font-bold font-serif">
+              {pattern.name}
+            </h1>
+          </div>
+          <div>
+            <p className="text-4xl leading-snug font-bold font-serif">
+              {pattern.price !== 0 ? convertCents(pattern.price) : 'Free'}
+            </p>
+          </div>
+        </div>
+
+        <div className="mb-8">
+          <p className="text-2xl text-green font-bold">{pattern.creator}</p>
+        </div>
+
+        <div className="mb-10">
+          <p className="text-lg">{pattern.description}</p>
+        </div>
+
+        <Button className="w-full sm:w-2/5" border>
+          Add to cart
+        </Button>
+      </div>
+    </ProductListingLayout>
   );
 };
 
