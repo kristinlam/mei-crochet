729449a97c49659532cf3d6996ae1898f026be5f
Kristin Lam <klam623@gmail.com>
2023-03-14 18:42:46 -0400

    Refactor styles into separate CartItem component

---
+++ client/components/CartItem.js
@@ -0,0 +1,46 @@
+import React, { useContext } from 'react';
+import CartContext from '../context/cart';
+import { Link } from 'react-router-dom';
+import { convertCents } from '../helpers';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+
+const CartItem = ({ key, item }) => {
+  const { cartItems, removeItem, calculatePrice } = useContext(CartContext);
+
+  const handleRemoveItemClick = (item) => {
+    removeItem(item);
+  };
+
+  return (
+    <div className="flex border-b-2 border-green py-6">
+      <div className="basis-9/12">
+        <div className="flex items-center">
+          <Link to={`/shop/${key}`}>
+            <div className="w-28 sm:w-32">
+              <img
+                className="w-full	aspect-square object-cover rounded-xl"
+                src={item.image}
+              />
+            </div>
+          </Link>
+          <div className="px-6">
+            <Link to={`/shop/${item.id}`}>
+              <p className="font-serif font-bold text-xl sm:text-2xl">
+                {item.name}
+              </p>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="flex grow items-center justify-between space-x-6">
+        <p className="text-xl sm:text-2xl">{convertCents(item.price)}</p>
+        <AiOutlineCloseCircle
+          className="self-start text-2xl sm:text-3xl cursor-pointer"
+          onClick={() => handleRemoveItemClick(item)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- client/pages/CartPage.js
+++ client/pages/CartPage.js
@@ -1,62 +1,45 @@
 import React, { useEffect, useContext } from 'react';
 import CartContext from '../context/cart';
 import FullPageLayout from '../layouts/FullPageLayout';
+import CartItem from '../components/CartItem';
 import Button from '../components/Button';
 import { Link } from 'react-router-dom';
-import { convertCents } from '../helpers';
-import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 const CartPage = () => {
-  const { cartItems, removeItem } = useContext(CartContext);
+  const { cartItems, calculatePrice } = useContext(CartContext);
 
-  const handleRemoveItemClick = (item) => {
-    removeItem(item);
-  };
-
-  const renderedCartItems = cartItems.map((item) => (
-    <div key={item.id} className="flex border-b-2 border-green py-8">
-      <div className="basis-9/12">
-        <div className="flex items-center">
-          <Link to={`/shop/${item.id}`}>
-            <div className="w-28 sm:w-32">
-              <img
-                className="w-full	aspect-square object-cover rounded-xl"
-                src={item.image}
-              />
-            </div>
-          </Link>
-
-          <div className="px-6">
-            <Link to={`/shop/${item.id}`}>
-              <p className="font-serif font-bold text-xl sm:text-2xl">
-                {item.name}
-              </p>
-            </Link>
-          </div>
-        </div>
+  const checkoutInfo = (
+    <div className="flex flex-col items-end">
+      <div className="mb-2">
+        <p className="text-2xl">Total: {calculatePrice()}</p>
       </div>
-      <div className="flex grow items-center justify-between space-x-6">
-        <p className="text-xl sm:text-2xl">{convertCents(item.price)}</p>
-        <AiOutlineCloseCircle
-          className="self-start text-2xl sm:text-3xl cursor-pointer"
-          onClick={() => handleRemoveItemClick(item)}
-        />
-      </div>
+      <Button border>Checkout</Button>
     </div>
-  ));
+  );
+
+  const displayCartOrMsg =
+    cartItems.length > 0 ? (
+      <div>
+        <div className="border-b-4 border-green mb-6">
+          {cartItems.map((item) => (
+            <CartItem key={item.id} item={item} />
+          ))}
+        </div>
+        {checkoutInfo}
+      </div>
+    ) : (
+      <div className="text-center mt-14">
+        <p className="font-serif text-3xl mb-2">
+          There are no items in your cart.
+        </p>
+        <Link to="/shop">Continue shopping</Link>
+      </div>
+    );
 
   return (
     <FullPageLayout cartPage>
-      <h1 className="text-orange text-center mb-16">Your Cart</h1>
-
-      <div className="border-b-4 border-green mb-6">{renderedCartItems}</div>
-
-      <div className="flex flex-col items-end">
-        <div className="mb-2">
-          <p className="text-2xl">Total: Placeholder</p>
-        </div>
-        <Button border>Checkout</Button>
-      </div>
+      <h1 className="text-orange text-center mb-6">Your Cart</h1>
+      {displayCartOrMsg}
     </FullPageLayout>
   );
 };
