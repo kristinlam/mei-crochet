f8a0011641398a9925ac66b3391e89ae9814f60f
Kristin Lam <klam623@gmail.com>
2023-03-24 19:11:52 -0400

    Render content based on how user reaches confirmation page

--- client/pages/ConfirmationPage.js
+++ client/pages/ConfirmationPage.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import FullPageLayout from '../layouts/FullPageLayout';
 
-const ConfirmationPage = (props) => {
-  const { cartItems } = props.location.state;
+const ConfirmationPage = ({ location }) => {
   const currentOrder = useSelector((state) => state.singleOrder);
-
-  const renderedCartItems = cartItems.map((item) => (
-    <div key={item.id}>
-      <p>{item.name}</p>
-      <p>{item.link}</p>
-    </div>
-  ));
+  const cartItems = location.state?.cartItems; // received from checkout page, persists on refresh
 
   return (
     <FullPageLayout>
-      <h1 className="mb-6">Order #{currentOrder.id}</h1>
-      <div>
-        <p>Thank you for trying out Mei Crochet!</p>
-        <p>
-          Check out the talented artists featured on this site and support them
-          by buying their patterns through the links below!
-        </p>
-        {renderedCartItems}
-      </div>
+      {/* check that visitor was directed from /checkout page and did not refresh page  */}
+      {currentOrder.id ? (
+        <div>
+          <h1 className="mb-6">Order #{currentOrder.id}</h1>
+          <div>
+            <p>Thank you for trying out Mei Crochet!</p>
+            <p>
+              Check out the talented artists featured on this site and support
+              them by buying their patterns through the links below!
+            </p>
+            {cartItems.map((item) => (
+              <div key={item.id}>
+                <p>{item.name}</p>
+                <p>{item.link}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="text-center mt-14">
+          <p className="font-serif text-3xl mb-2">Your order was not found.</p>
+          <Link to="/shop">Continue shopping</Link>
+        </div>
+      )}
     </FullPageLayout>
   );
 };
