737549231c05844f49959209099c7f1e3d12a93e
Kristin Lam <klam623@gmail.com>
2023-03-25 04:15:59 -0400

    Add handleSubmit, remove delete button for Pattern

--- client/components/admin/Pattern.js
+++ client/components/admin/Pattern.js
@@ -5,37 +5,34 @@ import Button from '../Button';
 const Pattern = ({ pattern }) => {
   const [showEdit, setShowEdit] = useState(false);
 
-  const handleEditClick = () => {
-    console.log('edit clicked');
+  const handleEdit = () => {
+    setShowEdit(!showEdit);
   };
 
-  const handleDeleteClick = () => {
-    console.log('delete clicked');
+  const handleSubmit = () => {
+    setShowEdit(false);
   };
 
   return (
     <div>
       {showEdit ? (
-        <p>PatternEdit here</p>
+        <PatternEdit pattern={pattern} onSubmit={handleSubmit} />
       ) : (
         <div>
-          <p>ID: {pattern.id}</p>
-          <p>Name: {pattern.name}</p>
-          <p>Creator: {pattern.creator}</p>
-          <p>Description: {pattern.description}</p>
-          <p>Price: {pattern.price}</p>
-          <p>Image: {pattern.image}</p>
-          <p>Link: {pattern.link}</p>
+          <div>
+            <p>ID: {pattern.id}</p>
+            <p>Name: {pattern.name}</p>
+            <p>Creator: {pattern.creator}</p>
+            <p>Description: {pattern.description}</p>
+            <p>Price: {pattern.price}</p>
+            <p>Image: {pattern.image}</p>
+            <p>Link: {pattern.link}</p>
+          </div>
+          <Button border onClick={handleEdit}>
+            Edit
+          </Button>
         </div>
       )}
-      <div>
-        <Button border onClick={handleEditClick}>
-          Edit
-        </Button>
-        <Button border onClick={handleDeleteClick}>
-          Delete
-        </Button>
-      </div>
     </div>
   );
 };
