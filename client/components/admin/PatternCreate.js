35c45843fe30b6279848cedc7f3b6c61255cc8be
Kristin Lam <klam623@gmail.com>
2023-03-26 01:05:13 -0400

    Add ability to create patterns for admin

--- client/components/admin/PatternCreate.js
+++ client/components/admin/PatternCreate.js
@@ -1,7 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createPattern } from '../../store/patterns';
+import Button from '../Button';
 
 const PatternCreate = () => {
-  return <div>PatternCreate</div>;
+  const initialPattern = {
+    name: '',
+    creator: '',
+    description: '',
+    price: '',
+    image: '',
+    link: '',
+  };
+
+  const [newPattern, setNewPattern] = useState(initialPattern);
+  const [showModal, setShowModal] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event, pattern) => {
+    event.preventDefault();
+    dispatch(createPattern(pattern));
+    setNewPattern(initialPattern);
+    setShowModal(true);
+  };
+
+  return (
+    <form onSubmit={(event) => handleSubmit(event, newPattern)}>
+      <div>
+        <label>Name</label>
+        <input
+          type="text"
+          value={newPattern.name}
+          onChange={(event) =>
+            setNewPattern({
+              ...newPattern,
+              name: event.target.value,
+            })
+          }
+        />
+      </div>
+      <div>
+        <label>Creator</label>
+        <input
+          type="text"
+          value={newPattern.creator}
+          onChange={(event) =>
+            setNewPattern({
+              ...newPattern,
+              creator: event.target.value,
+            })
+          }
+        />
+      </div>
+      <div>
+        <label>Description</label>
+        <textarea
+          value={newPattern.description}
+          onChange={(event) =>
+            setNewPattern({
+              ...newPattern,
+              description: event.target.value,
+            })
+          }
+        />
+      </div>
+      <div>
+        <label>Price</label>
+        <input
+          type="number"
+          value={newPattern.price}
+          onChange={(event) =>
+            setNewPattern({
+              ...newPattern,
+              price: event.target.value,
+            })
+          }
+        />
+      </div>
+      <div>
+        <label>Image</label>
+        <input
+          type="text"
+          value={newPattern.image}
+          onChange={(event) =>
+            setNewPattern({
+              ...newPattern,
+              image: event.target.value,
+            })
+          }
+        />
+      </div>
+      <div>
+        <label>Link</label>
+        <input
+          type="text"
+          value={newPattern.link}
+          onChange={(event) =>
+            setNewPattern({
+              ...newPattern,
+              link: event.target.value,
+            })
+          }
+        />
+      </div>
+
+      <Button border>Add</Button>
+      {showModal && (
+        <div>
+          <div>
+            <p>Pattern created successfully.</p>
+            <button onClick={() => setShowModal(false)}>OK</button>
+          </div>
+        </div>
+      )}
+    </form>
+  );
 };
 
 export default PatternCreate;
