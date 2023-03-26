66501f6c2703a7d9a97bb72f2c7d39e8453552ff
Kristin Lam <klam623@gmail.com>
2023-03-26 00:10:28 -0400

    Add PUT and DELETE routes for users, update slice

--- client/store/users.js
+++ client/store/users.js
@@ -1,29 +1,63 @@
 import axios from 'axios';
 
+const TOKEN = 'token';
 const GET_USERS = 'GET_USERS';
+const UPDATE_USER = 'UPDATE_USER';
+const DELETE_USER = 'DELETE_USER';
 
-const _getUsers = (users) => {
-  return {
-    type: GET_USERS,
-    users,
-  };
+const _getUsers = (users) => ({
+  type: GET_USERS,
+  users,
+});
+
+const _updateUser = (user) => ({
+  type: UPDATE_USER,
+  user,
+});
+
+const _deleteUser = (user) => ({
+  type: DELETE_USER,
+  user,
+});
+
+export const getUsers = () => async (dispatch) => {
+  const token = window.localStorage.getItem(TOKEN);
+  const { data } = await axios.get('/api/users', {
+    headers: { authorization: token },
+  });
+  dispatch(_getUsers(data));
 };
 
-export const getUsers = () => {
-  const token = window.localStorage.getItem('token');
+export const updateUser = (user) => async (dispatch) => {
+  const token = localStorage.getItem(TOKEN);
+  const { data } = await axios.put(`/api/users/${user.id}`, user, {
+    headers: {
+      authorization: token,
+    },
+  });
+  dispatch(_updateUser(data));
+};
 
-  return async (dispatch) => {
-    const { data: users } = await axios.get('/api/users', {
-      headers: { authorization: token },
-    });
-    dispatch(_getUsers(users));
-  };
+export const deleteUser = (id) => async (dispatch) => {
+  const token = localStorage.getItem(TOKEN);
+  const { data } = await axios.delete(`/api/patterns/${id}`, {
+    headers: {
+      authorization: token,
+    },
+  });
+  dispatch(_deletePattern(data));
 };
 
 export default function (state = [], action) {
   switch (action.type) {
     case GET_USERS:
       return action.users;
+    case UPDATE_USER:
+      return state.map((user) => {
+        return user.id === action.user.id ? action.user : user;
+      });
+    case DELETE_USER:
+      return state.filter((user) => user.id !== action.user.id);
     default:
       return state;
   }
--- server/api/users.js
+++ server/api/users.js
@@ -6,12 +6,35 @@ const { requireToken, isAdmin } = require('./middleware');
 
 module.exports = router;
 
+// Admin Routes
+// GET /api/users/
 router.get('/', requireToken, isAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
       attributes: ['id', 'username', 'isAdmin'], // attributes that are sent back
     });
-    res.json(users);
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PUT /api/users/:id
+router.put(':/id', requireToken, isAdmin, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    res.status(200).json(await user.update(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
+
+// DELETE /api/users/:id
+router.delete('/:id', requireToken, isAdmin, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    await user.destroy();
+    res.status(200).json(user);
   } catch (err) {
     next(err);
   }
